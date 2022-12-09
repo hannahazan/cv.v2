8a7157bd70a63406cbfb68b13522acce90645590
hannahazan <hannahazan2@gmail.com>
2022-12-09 14:35:27 +0100

    header+responsive 100%

--- mon-cv/src/pages/landingPage.js
+++ mon-cv/src/pages/landingPage.js
@@ -1,6 +1,7 @@
 import '../css/index.css'
 import '../css/landingPage.css'
 import '../css/mobileResponsive.css'
+import cross from '../pictures/xmark-solid.svg'
 import { Home } from 'tabler-icons-react';
 import { Code } from 'tabler-icons-react';
 import { Database } from 'tabler-icons-react';
@@ -8,48 +9,100 @@ import { DeviceMobile } from 'tabler-icons-react';
 import { PencilPlus } from 'tabler-icons-react';
 import { Menu2 } from 'tabler-icons-react';
 export default function Landing(){
+   
+    const changeClassMenu=()=>{
+        document.getElementById('navListrespon').className ="responsivenavList2"
+        document.getElementById('contMenu').className="containerMenu2"
+        document.getElementById('Cross').className='cross2'
+        document.getElementById('Line1').className="line2";
+        document.getElementById('Line2').className="line2";
+        document.getElementById('Line3').className="line2";
+        document.getElementById('Line4').className="line2";
+        document.getElementById('Line5').className="line2";    
+    }
+    const changeClassCross=()=>{
+        document.getElementById('navListrespon').className ="responsivenavList";
+        document.getElementById('contMenu').className="containerMenu"
+        document.getElementById('Cross').className='cross'
+    }
+   
+   window.addEventListener("resize",(event) => {
+        if(window.innerWidth>=820){
+            document.getElementById('navListrespon').className ="responsivenavList"
+            document.getElementById('Cross').className='cross'
+    
+        }
+        else{
+            document.getElementById('contMenu').className="containerMenu"
+        }
+    })
     return(
-   <div class="all">    
+   <div className="all">     
         <header>
-            <div class="displayFiltre"> 
-                <nav class="navBar">
+            <div className="displayFiltre"> 
+                <nav className="navBar">
                     <Home
                     size={30}
                     strokeWidth={2}
                     color={'#FFB8B8'}
                     className='home'
                     />
-                    <ul class="navList" id="navListrespon">
+                    <ul className="navList">
                         <li>À propos</li>
                         <li>Compétences</li>
                         <li>Parcours</li>
                         <li>Réalisations</li>
                         <li>Contact</li>
                     </ul>
-                    <Menu2
+                    <div className='containerMenu' id="contMenu"><Menu2
                     size={48}
                     strokeWidth={2}
                     color={'#FFB8B8'}
                     className='menu'
+                    onClick={changeClassMenu}
                     />
+                    </div>
+                    <img src={cross} className='cross' id="Cross" onClick={changeClassCross}></img>
                 </nav>
-                <section class="devP">
-                    <div class="devPblock1">
+                <ul className="responsivenavList"  id="navListrespon">
+                    <li className='responLi'>
+                        À propos   
+                    </li>
+                    <div className='line' id="Line1"></div>
+                    <li className='responLi'>
+                        Compétences
+                    </li>
+                    <div className='line' id="Line2"></div>
+                    <li className='responLi'>
+                        Parcours
+                    </li>
+                    <div className='line' id="Line3"></div>
+                    <li className='responLi'>
+                        Réalisations
+                    </li>
+                    <div className='line' id="Line4"></div>
+                    <li className='responLi'>
+                        Contact
+                    </li>
+                    <div className='line'id="Line5"></div>
+                </ul>
+                <section className="devP">
+                    <div className="devPblock1">
                         <p>Bonjour, je m'appelle Hannah et je recherche</p>
                     </div>
-                    <div class="devPblock2">
-                        <p class="devPblock2Ele">une</p>
-                        <p class="devPblock2Ele" id="alter">Alternance</p>
-                        <p class="devPblock2Ele"> en tant que</p>
-                        <p class="devPblock2Ele" id="dev">Développeuse Web Full-stack</p>    
+                    <div className="devPblock2">
+                        <p className="devPblock2Ele">une</p>
+                        <p className="devPblock2Ele" id="alter">Alternance</p>
+                        <p className="devPblock2Ele"> en tant que</p>
+                        <p className="devPblock2Ele" id="dev">Développeuse Web Full-stack</p>    
                     </div>
-                    <p class="devPblock2Resp">une Alternance en tant que Développeuse web Full-stack</p>
+                    <p className="devPblock2Resp">une Alternance en tant que Développeuse web Full-stack</p>
                     <div>
                         <p>sur Paris et sa région</p>
                     </div>
                 </section>
-                <section class="picto">
-                    <div class="pictoAndP">
+                <section className="picto">
+                    <div className="pictoAndP">
                         <Code
                         size={48}
                         strokeWidth={2}
@@ -57,7 +110,7 @@ export default function Landing(){
                         />
                         <p>Front-end</p>
                     </div>
-                    <div class="pictoAndP">
+                    <div className="pictoAndP">
                         <Database
                         size={48}
                         strokeWidth={2}
@@ -65,7 +118,7 @@ export default function Landing(){
                         />
                         <p>Back-end</p>
                     </div>
-                    <div class="pictoAndP">
+                    <div className="pictoAndP">
                         <DeviceMobile
                         size={48}
                         strokeWidth={2}
@@ -73,7 +126,7 @@ export default function Landing(){
                         />
                         <p>Mobile</p>
                     </div>
-                    <div class="pictoAndP">
+                    <div className="pictoAndP">
                         <PencilPlus
                         size={48}
                         strokeWidth={2}
